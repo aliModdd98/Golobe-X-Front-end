4b9ebd1abb556965cc3297bc8685a4358d404598
aliModdd98 <https://github.com/aliModdd98/>
2023-11-26 21:25:46 +0300

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import FlightDetails from './pages/FlightDetails'
 import HotelListing from './pages/HotelListing'
 
 function App() {
   
   return (
   <>
-< HotelListing/> </>
+< FlightDetails/> </>
      )
 }
 
